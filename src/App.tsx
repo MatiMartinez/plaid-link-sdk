37373f7d84fd22b4f1b76acc12ec1cc394465185
MatiiMartinez <matiase.martinez@aden.org>
2021-07-06 18:08:11 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, useState, useCallback } from "react";
+import styled from "styled-components";
+import { usePlaidLink, PlaidLinkOptions, PlaidLinkOnSuccess } from "react-plaid-link";
+import { Button, TextField } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+	token: string;
 }
 
+const PlaidLink: FunctionComponent<Props> = ({ token }) => {
+	const onSuccess = useCallback<PlaidLinkOnSuccess>((public_token, metadata) => {
+		// send public_token to server
+	}, []);
+
+	const config: PlaidLinkOptions = {
+		token,
+		onSuccess,
+		// onExit
+		// onEvent
+	};
+
+	const { open, ready, error } = usePlaidLink(config);
+
+	console.log(error, open, ready);
+
+	return (
+		<Button variant="contained" color="primary" onClick={() => open()} disabled={!ready}>
+			Conectar
+		</Button>
+	);
+};
+
+const App: FunctionComponent = () => {
+	const [auxToken, setAuxToken] = useState<string>("");
+	const [token, setToken] = useState<string | null>(null);
+
+	const handleSubmit = () => setToken(auxToken);
+
+	const handleClean = () => {
+		setToken(null);
+		setAuxToken("");
+	};
+
+	return (
+		<div className="App">
+			<Container>
+				<Text>Ingresar Token</Text>
+				<TextField value={auxToken} onChange={(e) => setAuxToken(e.target.value)} />
+				<Button variant="contained" color="primary" fullWidth onClick={handleSubmit}>
+					Enviar
+				</Button>
+				<Button variant="outlined" color="primary" fullWidth onClick={handleClean}>
+					Limpiar
+				</Button>
+				{token === null ? <></> : <PlaidLink token={token} />}
+			</Container>
+		</div>
+	);
+};
+
 export default App;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	row-gap: 2rem;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+const Text = styled.h1`
+	font-size: 2rem;
+`;
